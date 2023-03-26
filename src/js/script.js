badf586b355995c8ec3cdf96c30c24a15ed4ebfd
NikitaYanakiev <yanakiiev@gmail.com>
2023-03-26 10:49:36 +0300

    add project

--- src/js/script.js
+++ src/js/script.js
@@ -11,12 +11,6 @@ burger.addEventListener('click', () => {
     body.classList.toggle('lock');
 });
 
-// closeElem.addEventListener('click', () => {
-//     menu.classList.remove('active');
-//     burger.classList.remove('active');
-//     body.classList.remove('lock');
-// });
-
 
 overlay.addEventListener('click', () => {
     menu.classList.remove('active');
@@ -38,59 +32,59 @@ menuLinks.forEach(link => {
 
 
 
-    function digitsCountersInit(digitsCountersItems) {
-        let digitsCounters = 
-        digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-digits-counter]");
-        if (digitsCounters) {
-            digitsCounters.forEach(digitsCounter => {
-                digitsCountersAnimate(digitsCounter);
-            });
+function digitsCountersInit(digitsCountersItems) {
+    let digitsCounters = 
+    digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-digits-counter]");
+    if (digitsCounters) {
+        digitsCounters.forEach(digitsCounter => {
+            digitsCountersAnimate(digitsCounter);
+        });
+    }
+}
+
+function digitsCountersAnimate(digitsCounter) {
+    let startTimestamp = null;
+    const duration = 
+    parseInt(digitsCounter.dataset.digitsCounter) ? parseInt(digitsCounter.dataset.digitsCounter) : 2000;
+    const startValue = parseInt(digitsCounter.innerHTML);
+    let startPosition = 0;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        digitsCounter.innerHTML = (Math.floor(progress * (startPosition + startValue))) + '%';
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
         }
-    }
-
-    function digitsCountersAnimate(digitsCounter) {
-        let startTimestamp = null;
-        const duration = 
-        parseInt(digitsCounter.dataset.digitsCounter) ? parseInt(digitsCounter.dataset.digitsCounter) : 2000;
-        const startValue = parseInt(digitsCounter.innerHTML);
-        let startPosition = 0;
-        const step = (timestamp) => {
-            if (!startTimestamp) startTimestamp = timestamp;
-            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-            digitsCounter.innerHTML = (Math.floor(progress * (startPosition + startValue))) + '%';
-            if (progress < 1) {
-                window.requestAnimationFrame(step);
-            }
-        };
-        window.requestAnimationFrame(step);
-
-        const counters = document.querySelectorAll('.skills__ratings-counter'),
-            lines = document.querySelectorAll('.skills__ratings-line span');
-
-        counters.forEach( (item, i) => {
-            lines [i].style.width = item.innerHTML;
-            lines [i].style.left = 0;
-        });
-    }
-    
-
-    let options = {
-        threshold: 0.8
     };
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const targetElement = entry.target;
-                const digitsCountersItems = targetElement.querySelectorAll ("[data-digits-counter]");
-                if (digitsCountersItems.length) {
-                    digitsCountersInit(digitsCountersItems);
-                }
-                observer.unobserve(targetElement);
-            }
-        });
-    }, options);
+    window.requestAnimationFrame(step);
 
-    let section = document.querySelectorAll('.skills__ratings');
+    const counters = document.querySelectorAll('.skills__ratings-counter'),
+        lines = document.querySelectorAll('.skills__ratings-line span');
+
+    counters.forEach( (item, i) => {
+        lines [i].style.width = item.innerHTML;
+        lines [i].style.left = 0;
+    });
+}
+
+
+let options = {
+    threshold: 0.8
+};
+let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const targetElement = entry.target;
+            const digitsCountersItems = targetElement.querySelectorAll ("[data-digits-counter]");
+            if (digitsCountersItems.length) {
+                digitsCountersInit(digitsCountersItems);
+            }
+            observer.unobserve(targetElement);
+        }
+    });
+}, options);
+
+let section = document.querySelectorAll('.skills__ratings');
     if (section.length) {
         section.forEach(section => {
             observer.observe(section);
